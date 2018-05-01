3e82d93185d8709a458477ab3d03689e43d2e7da
Ronald Schlenker <github@screen9.de>
2018-05-01 10:39:31 +0200

    Implemented result type as value,aggregatedState and globalState

--- src/FSiren.ConsoleTest/Program.fs
+++ src/FSiren.ConsoleTest/Program.fs
@@ -1,20 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open FSiren.Core
+open FSiren
+
+type GlobalState = {data:string}
 
 [<EntryPoint>]
 let main argv =
-    
+
     let comp = circuit {
         let! x = counter 1 1
         let! y = counter 11 1
         return sprintf "x=%A y=%A" x y
     }
 
-    let c1 = comp.Run None
-    let c2 = comp.Run (Some c1)
-    let c3 = comp.Run (Some c2)
-    let c4 = comp.Run (Some c3)
+    let v1,s1,g1 = (run comp) None {data = "Some global state"}
+    let v2,s2,g2 = (run comp) (Some s1) g1
+    let v3,s3,g3 = (run comp) (Some s2) g2
+    let v4,s4,g4 = (run comp) (Some s3) g3
+
+    printfn "v1: %A" v1
+    printfn "v2: %A" v2
+    printfn "v3: %A" v3
+    printfn "v4: %A" v4
 
     0
--- src/FSiren/SirenMonad.fs
+++ src/FSiren/SirenMonad.fs
@@ -1,33 +1,91 @@
-module FSiren.Core
+// Origin: "DAS IST ES - Ma_a_Mb_Mc (2).fsx"
 
-type M<'a, 'g> = 
-    | M of ((Option<'a> * 'g) -> ('a * 'g))
-    member x.Run = match x with M b -> b
+module FSiren
 
-// Builder
+type M<'a, 'TState, 'TGlobalState> =
+    | M of ('TState option -> 'TGlobalState -> 'a * 'TState * 'TGlobalState)
+let run m = match m with | M x -> x
+
 type CircuitBuilder() =
-    member this.Bind(m:M<'a, 'g>, f:('a -> M<'b, 'g>)) : M<('a * 'b), 'g> =
-        M(fun wholeState ->
-            let aOpt, globalState = wholeState
-            let a', b' =
-                match aOpt with
-                | None -> None, None
-                | Some (a,b) -> Some a, Some b
 
-            let mResValue, mResGlobalState = m.Run (a', globalState)
-            let mF = f mResValue
-            let fResValue, fResGlobalState = mF.Run (b', mResGlobalState)
-            (mResValue, fResValue), fResGlobalState
-        )
-    member this.Return x = M(fun (_, globalState) -> (x, globalState))
+    member this.Bind (
+                      m: M<'a, 'AState, 'TGlobalState>, 
+                      f: ('a -> M<'b, 'BState, 'TGlobalState>))  
+            : M<'b, ('AState * 'BState), 'TGlobalState> =
+        
+        let stateFunc localState globalState =
+            let prevAState, prevBState = match localState with
+                                            | None -> (None, None)
+                                            | Some (a,b) -> (Some a, Some b)
+            let aValue,aState,mGlobalState = run m prevAState globalState
+            let fRes = f aValue
+            let bValue,bState,fGlobalState = run fRes prevBState mGlobalState
+            bValue,(aState,bState),fGlobalState
+        M stateFunc
+
+    member this.Return x = M(fun _ b -> (x,(),b))
 let circuit = CircuitBuilder()
 
 
-// helper
-let t1 seed acc =
-    M(function
-        | None, globalState -> seed, globalState
-        | Some localStateState, globalState -> acc localStateState, globalState)
+/////
+//// Convenience
+/////
+//type Cont<'v, 'g> =
+//    | Cont of (unit -> 'v * 'g * Cont<'v, 'g>)
+//    member x.Eval = match x with Cont b -> b
+//let start (m:M<_,_,_>) =
+//    let rec evalInternal oldState globalState = 
+//        let v,newS,newG = run m oldState globalState
+//        (v, newG, Cont(fun() -> evalInternal (Some newS) newG))
+//    evalInternal None
 
-// domain instances
-let counter seed inc = t1 seed (fun prev -> prev + inc)
+
+///
+// Helper
+///
+
+type F<'v, 's, 'g> = ('s -> 'g -> ('v * 's * 'g))
+
+// a : value as state
+// b : no global state
+// c : read only global state
+let lift_a (f:('a * 'b) -> 'g -> ('a * 'b)) =
+    fun prev globalState ->
+        let fVal,newGlobalState = f prev globalState
+        (fVal,fVal,newGlobalState)
+let lift_ab (f:'v -> 'v) =
+    fun prev globalState ->
+        let fVal = f prev
+        (fVal,fVal,globalState)
+let lift_ac (f:'s -> 'g -> 'v) =
+    fun prev globalState ->
+        let fVal = f prev globalState
+        (fVal,fVal,globalState)
+let lift_b (f:'s -> ('v * 's)) =
+    fun prev globalState ->
+        let fVal,fState = f prev
+        (fVal,fState,globalState)
+let lift_c (f:'s -> 'g -> ('v * 's)) =
+    fun prev globalState ->
+        let fVal,fState = f prev globalState
+        (fVal,fState,globalState)
+
+// delayed value
+let t1 acc seed  =
+    M(
+        fun prev globalState ->
+            let x = match prev with
+                    | Some previousState -> previousState
+                    | None -> seed
+            acc x globalState
+    )
+
+// Usage
+let inline counter inc seed =
+    let f = ((fun prev -> prev + inc) |> lift_ab)
+    t1 f seed
+let toggle seed = 
+        t1 
+        <| ((fun prev -> if (prev = true) then (0,false) else (1, true)) |> lift_b)
+        <| seed
+ 
