fe06109200922e2c6b26ae09b38a5ac51aba1c62
Ronald Schlenker <github@screen9.de>
2018-05-03 11:11:16 +0200

    moved stuff

---
+++ test.fsx
@@ -0,0 +1,12 @@
+﻿#r "bin\Debug\FSiren.dll"
+#r "bin\Debug\FSiren.NAudio.dll"
+
+open FSiren
+open FSiren.Playback
+  
+let comp = circuit {
+    let! r = random()
+        return r
+}
+
+playSync comp 2.0<s> |> ignore
