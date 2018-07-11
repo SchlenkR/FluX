c977cf28c556bfd41c7405633e90356f7df02778
Ronald Schlenker <github@screen9.de>
2018-07-11 12:13:18 +0300

    Filter added

--- src/FLooping/lib/Blocks/Filters.fs
+++ src/FLooping/lib/Blocks/Filters.fs
@@ -125,97 +125,79 @@ module Filters =
             { biQuadCoeffsZero with a0=a0; a1=a1; a2=a2; b1=b1; b2=b2 }
         biQuadBase input p calcCoeffs
 
-
-        // protected override void CalculateBiQuadCoefficients()
-        // {
-        //     const double sqrt2 = 1.4142135623730951;
-        //     double k = Math.Tan(Math.PI * Frequency / SampleRate);
-        //     double v = Math.Pow(10, Math.Abs(GainDB) / 20.0);
-        //     double norm;
-        //     if (GainDB >= 0)
-        //     {    // boost
-        //         norm = 1 / (1 + sqrt2 * k + k * k);
-        //         A0 = (1 + Math.Sqrt(2 * v) * k + v * k * k) * norm;
-        //         A1 = 2 * (v * k * k - 1) * norm;
-        //         A2 = (1 - Math.Sqrt(2 * v) * k + v * k * k) * norm;
-        //         B1 = 2 * (k * k - 1) * norm;
-        //         B2 = (1 - sqrt2 * k + k * k) * norm;
-        //     }
-        //     else
-        //     {    // cut
-        //         norm = 1 / (1 + Math.Sqrt(2 * v) * k + v * k * k);
-        //         A0 = (1 + sqrt2 * k + k * k) * norm;
-        //         A1 = 2 * (k * k - 1) * norm;
-        //         A2 = (1 - sqrt2 * k + k * k) * norm;
-        //         B1 = 2 * (v * k * k - 1) * norm;
-        //         B2 = (1 - Math.Sqrt(2 * v) * k + v * k * k) * norm;
-        //     }
-        // }
-    // let lowShelfDef = { frq=1000.0; q=1.0; gain=0.0 }
-    // let lowShelf input (p:BiQuadParams) =
-    //     let calcCoeffs (env:Env) =
-    //         let k = Math.Tan(Math.PI * p.frq / float env.sampleRate)
-    //         let v = Math.Pow(10.0, Math.Abs(p.gain) / 20.0)
-    //         match p.gain >= 0.0 with
-    //         | true ->
-    //             // boost
-    //             let norm = 1.0 / (1.0 + sqrt2 * k + k * k)
-    //             { biQuadCoeffsZero with 
-    //                 a0 = (v + Math.Sqrt(2.0 * v) * k + k * k) * norm
-    //                 a1 = 2.0 * (k * k - v) * norm
-    //                 a2 = (v - Math.Sqrt(2.0 * v) * k + k * k) * norm
-    //                 b1 = 2.0 * (k * k - 1.0) * norm
-    //                 b2 = (1.0 - sqrt2 * k + k * k) * norm
-    //             }
-    //         | false ->
-    //             // cut
-    //             let norm = 1.0 / (v + Math.Sqrt(2.0 * v) * k + k * k)
-    //             { biQuadCoeffsZero with 
-    //                 a0 = (1.0 + sqrt2 * k + k * k) * norm
-    //                 a1 = 2.0 * (k * k - 1.0) * norm
-    //                 a2 = (1.0 - sqrt2 * k + k * k) * norm
-    //                 b1 = 2.0 * (k * k - v) * norm
-    //                 b2 = (v - Math.Sqrt(2.0 * v) * k + k * k) * norm
-    //             }
-    //     biQuadBase input p calcCoeffs
+    let lowShelfDef = { frq=1000.0; q=1.0; gain=0.0 }
+    let lowShelf input (p:BiQuadParams) =
+        let calcCoeffs (env:Env) =
+            let k = Math.Tan(Math.PI * p.frq / float env.sampleRate)
+            let v = Math.Pow(10.0, Math.Abs(p.gain) / 20.0)
+            match p.gain >= 0.0 with
+            | true ->
+                // boost
+                let norm = 1.0 / (1.0 + sqrt2 * k + k * k)
+                { biQuadCoeffsZero with 
+                    a0 = (1.0 + Math.Sqrt(2.0 * v) * k + v * k * k) * norm
+                    a1 = 2.0 * (v * k * k - 1.0) * norm
+                    a2 = (1.0 - Math.Sqrt(2.0 * v) * k + v * k * k) * norm
+                    b1 = 2.0 * (k * k - 1.0) * norm
+                    b2 = (1.0 - sqrt2 * k + k * k) * norm
+                }
+            | false ->
+                // cut
+                let norm = 1.0 / (1.0 + Math.Sqrt(2.0 * v) * k + v * k * k)
+                { biQuadCoeffsZero with 
+                    a0 = (1.0 + sqrt2 * k + k * k) * norm
+                    a1 = 2.0 * (k * k - 1.0) * norm
+                    a2 = (1.0 - sqrt2 * k + k * k) * norm
+                    b1 = 2.0 * (v * k * k - 1.0) * norm
+                    b2 = (1.0 - Math.Sqrt(2.0 * v) * k + v * k * k) * norm
+                }
+        biQuadBase input p calcCoeffs
 
 
+    let notchDef = { frq=1000.0; q=1.0; gain=0.0 }
+    let notch input (p:BiQuadParams) =
+        let calcCoeffs (env:Env) =
+            let k = Math.Tan(Math.PI * p.frq / float  env.sampleRate)
+            let norm = 1.0 / (1.0 + k / p.q + k * k)
+            let a0 = (1.0 + k * k) * norm
+            let a1 = 2.0 * (k * k - 1.0) * norm
+            let a2 = a0
+            let b1 = a1
+            let b2 = (1.0 - k / p.q + k * k) * norm
+            { biQuadCoeffsZero with a0=a0; a1=a1; a2=a2; b1=b1; b2=b2 }
+        biQuadBase input p calcCoeffs
 
-// NOtch:
-            // double k = Math.Tan(Math.PI * Frequency / SampleRate);
-            // double norm = 1 / (1 + k / Q + k * k);
-            // A0 = (1 + k * k) * norm;
-            // A1 = 2 * (k * k - 1) * norm;
-            // A2 = A0;
-            // B1 = A1;
-            // B2 = (1 - k / Q + k * k) * norm;
 
-
-
-// Peak:
-            // double norm;
-            // double v = Math.Pow(10, Math.Abs(GainDB) / 20.0);
-            // double k = Math.Tan(Math.PI * Frequency / SampleRate);
-            // double q = Q;
-
-            // if (GainDB >= 0) //boost
-            // {
-            //     norm = 1 / (1 + 1 / q * k + k * k);
-            //     A0 = (1 + v / q * k + k * k) * norm;
-            //     A1 = 2 * (k * k - 1) * norm;
-            //     A2 = (1 - v / q * k + k * k) * norm;
-            //     B1 = A1;
-            //     B2 = (1 - 1 / q * k + k * k) * norm;
-            // }
-            // else //cut
-            // {
-            //     norm = 1 / (1 + v / q * k + k * k);
-            //     A0 = (1 + 1 / q * k + k * k) * norm;
-            //     A1 = 2 * (k * k - 1) * norm;
-            //     A2 = (1 - 1 / q * k + k * k) * norm;
-            //     B1 = A1;
-            //     B2 = (1 - v / q * k + k * k) * norm;
-            // }
+    let peakDef = { frq=1000.0; q=1.0; gain=0.0 }
+    let peak input (p:BiQuadParams) =
+        let calcCoeffs (env:Env) =
+            let v = Math.Pow(10.0, Math.Abs(p.gain) / 20.0)
+            let k = Math.Tan(Math.PI * p.frq / float env.sampleRate)
+            let l = p.q * k + k * k
+            match p.gain >= 0.0 with
+            | true ->
+                // boost
+                let norm = 1.0 / (1.0 + 1.0 / l)
+                let a1 = 2.0 * (k * k - 1.0) * norm
+                { biQuadCoeffsZero with
+                    a0 = (1.0 + v / l) * norm
+                    a1 = a1
+                    a2 = (1.0 - v / l) * norm
+                    b1 = a1
+                    b2 = (1.0 - 1.0 / l) * norm
+                }
+            | false ->
+                // cut
+                let norm = 1.0 / (1.0 + v / l)
+                let a1 = 2.0 * (k * k - 1.0) * norm
+                { biQuadCoeffsZero with 
+                    a0 = (1.0 + 1.0 / l) * norm
+                    a1 = a1
+                    a2 = (1.0 - 1.0 / l) * norm
+                    b1 = a1
+                    b2 = (1.0 - v / l) * norm
+                }
+        biQuadBase input p calcCoeffs
 
 
 
